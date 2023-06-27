4dc717b6573c79364e53b544919e30f862e276d9
HANYUJIN <mooo7570@gmail.com>
2023-06-27 22:26:49 +0900

    0627

---
+++ JavaScript/pokemonPJ/app.js
@@ -0,0 +1,16 @@
+//http://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+
+const container = document.querySelector('#container');
+const baseURL =
+  'http://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+
+for (let i = 1; i <= 151; i++) {
+  const pokemon = document.createElement('div');
+  const newImg = document.createElement('img');
+  const label = document.createElement('span');
+  label.innerText = `#${i}`;
+  newImg.src = `${baseURL}${i}.png`;
+  pokemon.appendChild(newImg);
+  pokemon.appendChild(label);
+  container.appendChild(pokemon);
+}
